5e0698522364b3b445963470f8a16ceffc512adb
Jeremy Eudy <jeremyeudy@gmail.com>
2019-11-23 17:05:57 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Secret-Santa
-A Python utility for creating Secret Santa matches
+A Python utility for creating Secret Santa matches.
+
+This script takes in a list of names and emails and then matches them up into pairs, and sends that pairing information to the gift giver. A master copy is then stored on the server, and can optionally be sent to another email address.
+
+## Getting Started
+Easy clone:
+```
+git clone https://github.com/JeremyEudy/Secret-Santa
+```
+
+## Usage
+Load names and emails from structured input files (currently supports .json and .csv), or manually input users.
+
+```
+python3 Secret-Santa.py -t <file type> -i <input file> -m <master email destination>
+```
+
+Input files need to be structured the same way as the sample files in order to be parsed correctly.
---
+++ Secret-Santa.py
@@ -0,0 +1,70 @@
+# Author: Jeremy Eudy
+import sys, getopt, os
+import json, csv
+
+def printHeader():
+    print("###################################################")
+    print("#                                                 #")
+    print("#             Welcome to Secret Santa             #")
+    print("#                                                 #")
+    print("###################################################")
+    return
+
+def silent(argv):
+    if len(argv) == 0:
+        return
+    inputFile = ''
+    fileType = ''
+    try:
+        opts, args = getopt.getopt(argv,"ht:i:",["ifile=","ftype="])
+    except getopt.GetoptError:
+        print('usage: Secret-Santa.py -t <file type> -i <input file>')
+        print('\tSupported file types: .json, .txt, .csv')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('usage: Secret-Santa.py -t <file type> -i <input file>')
+            print('\tSupported file types: json, csv')
+            sys.exit()
+        elif opt in ("-t", "--ftype"):
+            fileType = arg
+        elif opt in ("-i", "--ifile"):
+            inputFile = arg
+
+    if inputFile:
+        participants = {}
+        with open("inputFile", "r") as f:
+            if fileType == "json":
+                participants = json.loads(f.read())
+            elif fileType == "csv":
+                csvReader = csv.reader(f)
+                for row in csvReader:
+                    participants[row[0]] = row[1]
+
+        print(participants)
+
+def manual():
+    printHeader()
+    participants = {}
+    print("Input the name of each participant (enter 'q' when done)")
+    while(True):
+        person = input(">")
+        if person == "q":
+            break
+        else:
+            participants[person] = "None"
+
+    if len(participants) == 0:
+        sys.exit()
+
+    print("Input the email of each participant")
+    for person in participants.keys():
+        email = input("{}\n>".format(person))
+        participants[person]= email
+
+    print(participants)
+
+if __name__ == "__main__":
+    os.system("clear")
+    silent(sys.argv[1:])
+    manual()
